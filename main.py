5310afcdbbfbeb894dcc2fb748dbff5822951ced
Amishaj13 <amishajoshi1306@gmail.com>
2025-04-04 18:33:07 +0530

    made required changes

--- main.py
+++ main.py
@@ -43,4 +43,4 @@ def edit_image():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))  # Render sets PORT env var
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
